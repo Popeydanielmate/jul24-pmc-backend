f84edeb9cac0218801761d692a6f7863bc0b0508
Popeydanielmate <14269@coderacademy.edu.au>
2024-08-03 14:59:05 +1000

    fix: deleted console log

--- utils/emailService.js
+++ utils/emailService.js
@@ -1,12 +1,9 @@
 const nodemailer = require('nodemailer');
 
-console.log('EMAIL_USER:', process.env.EMAIL_USER);
-console.log('EMAIL_PASS:', process.env.EMAIL_PASS);
-
 const transporter = nodemailer.createTransport({
     host: 'smtp.office365.com',
     port: 587,
-    secure: false, // true for 465, false for other ports
+    secure: false, 
     auth: {
       user: process.env.EMAIL_USER,
       pass: process.env.EMAIL_PASS,
