db1850997d0587764a767f9309784e9c1283036d
Popeydanielmate <14269@coderacademy.edu.au>
2024-08-08 22:30:18 +1000

    feat: get user route for collection items

--- routes/auth.js
+++ routes/auth.js
@@ -181,5 +181,16 @@ router.post('/reset-password', async (req, res) => {
   }
 });
 
+router.get('/user', auth, async (req, res) => {
+    try {
+      const user = await User.findById(req.user.id).select('-password');
+      res.json(user);
+    } catch (err) {
+      console.error('Server error:', err.message);
+      res.status(500).send('Server error');
+    }
+  });
+  
+
 module.exports = router;
 
