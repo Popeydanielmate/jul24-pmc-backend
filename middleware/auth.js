9108be5196ca7cc4ccc3d88f5428f6961ca37341
Popeydanielmate <14269@coderacademy.edu.au>
2024-09-07 19:58:20 +1000

    fix: fetches correct token now so collection items can be added

--- middleware/auth.js
+++ middleware/auth.js
@@ -1,7 +1,9 @@
 const jwt = require('jsonwebtoken');
 
 function auth(req, res, next) {
-  const token = req.header('x-auth-token');
+  
+  const authHeader = req.header('Authorization');
+  const token = authHeader && authHeader.split(' ')[1]; 
 
   if (!token) {
     return res.status(401).json({ message: 'No token, authorization denied' });
@@ -10,8 +12,10 @@ function auth(req, res, next) {
   try {
     const decoded = jwt.verify(token, process.env.JWT_SECRET);
     req.user = decoded.user;
+    console.log('Authenticated user:', req.user); 
     next();
   } catch (err) {
+    console.error('Token verification failed:', err); 
     res.status(401).json({ message: 'Token is not valid' });
   }
 }
